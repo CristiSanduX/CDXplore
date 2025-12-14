ce4cc1601cc5fd6d5d5fb8d24001f5e5d4b6d428
Cristi Sandu <cristi.sandu@csx.ro>
2025-12-14 20:32:43 +0200

    [iOS] Initial iOS project

---
+++ mobile-ios/CDXplore/CDXplore/CDXploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDXploreApp.swift
+//  CDXplore
+//
+//  Created by Cristi Sandu on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CDXploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
